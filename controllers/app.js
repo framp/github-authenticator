a83eec91423e43be2d5c978bfdf551830d3422fb
Federico Rampazzo <frampone@gmail.com>
2014-11-21 01:52:01 +0000

    fix rest controllers

--- controllers/app.js
+++ controllers/app.js
@@ -2,26 +2,42 @@ var App = require('../models/app');
 
 module.exports = function(app){
   app.post('/apps', function(req, res, next){
-    App
-    .create(req.body)
-    .success(function(app) {
+    App.create(req.body)
+    .then(function(app) {
       res.send(201, app.values);
-    })
+    }, function(err){
+      console.error(err);
+      res.send(500);
+    });
   });
   
-  app.get('/apps/:client_id', function(){
-    App
-    .find({ client_id: req.body.client_id })
-    .success(function(app) {
+  app.put('/apps/:client_id', function(req, res, next){
+    App.update(req.body, { client_id: req.body.client_id })
+    .then(function(app) {
       res.send(200, app.values);
-    })
+    }, function(err){
+      console.error(err);
+      res.send(500);
+    });
   });
   
-  app.get('/callback/:client_id', function(){
-    App
-    .find({ client_id: req.body.client_id })
-    .success(function(app) {
-      
-    })
+  app.get('/apps/:client_id', function(req, res, next){
+    App.find({ client_id: req.body.client_id })
+    .then(function(app) {
+      res.send(200, app.values);
+    }, function(err){
+      console.error(err);
+      res.send(500);
+    });
+  });
+  
+  app.get('/callback/:client_id', function(req, res, next){
+    App.find({ client_id: req.body.client_id })
+    .then(function(app) {
+      res.send(200, app.values);
+    }, function(err){
+      console.error(err);
+      res.send(500);
+    });
   });
 }
