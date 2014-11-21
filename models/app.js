62e6d60504f23cd4d899bc5a3b29cdd0602de8a5
Federico Rampazzo <frampone@gmail.com>
2014-11-21 04:13:43 +0000

    model validation

--- models/app.js
+++ models/app.js
@@ -3,7 +3,23 @@ var sequelize = require('../lib/database');
 
 module.exports = sequelize.define('App', {
   name: Sequelize.STRING,
-  client_id: Sequelize.STRING,
-  client_secret: Sequelize.STRING,
-  callback: Sequelize.STRING
+  client_id: {
+    type: Sequelize.STRING,
+    unique: { name: 'client_id', msg: 'Client Id already registered.' },
+    validate: {
+      notEmpty: true
+    }
+  },
+  client_secret: {
+    type: Sequelize.STRING,
+    validate: {
+      notEmpty: true
+    }
+  },
+  callback: {
+    type: Sequelize.STRING,
+    validate: {
+      notEmpty: true
+    }
+  }
 });
